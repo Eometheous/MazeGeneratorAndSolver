8afac2007a3f68a73beb478e59fe697e24516e70
Eometheous <jonathan.sthomas@icloud.com>
2022-10-27 14:02:00 -0700

    added display functionality for maze

--- src/main/java/main/mazes/Cell.java
+++ src/main/java/main/mazes/Cell.java
@@ -1,15 +1,65 @@
 package main.mazes;
 
 public class Cell {
-    private static final String H_WALL = "---";
-    private static final String EMPTY_H_WALL = "   ";
+    private static final String H_WALL = "-----";
+    private static final String EMPTY_H_WALL = "     ";
     private static final String V_WALL = "|";
     private static final String EMPTY_V_WALL = " ";
     private static final String CORNER = "+";
 
-    boolean top, bottom, left, right;
+    private String top, bottom, left, right;
+    private boolean isEnd;
+    private int number;
 
-    public Cell(boolean top, boolean bottom, boolean left, boolean right) {
-
+    public Cell() {
+        number = -1;
+        top = H_WALL;
+        bottom = H_WALL;
+        left = V_WALL;
+        right = V_WALL;
+        isEnd = false;
     }
+    public String getTop() {
+        if (isEnd) return CORNER + top + CORNER + "\n";
+        return CORNER + top;
+    }
+
+    public String getMiddle() {
+        if (isEnd) return left + "     " + right + "\n";
+        return left + "     ";
+    }
+
+    public String getBottom() {
+        if (isEnd) return CORNER + bottom + CORNER + "\n";
+        return CORNER + bottom;
+    }
+
+    public void setTopConnection(boolean connection) {
+        if (connection) top = EMPTY_H_WALL;
+        else top = H_WALL;
+    }
+
+    public void setBottomConnection(boolean connection) {
+        if (connection) bottom = EMPTY_H_WALL;
+        else bottom = H_WALL;
+    }
+
+    public void setLeftConnection(boolean connection) {
+        if (connection) left = EMPTY_V_WALL;
+        else left = V_WALL;
+    }
+
+    public void setRightConnection(boolean connection) {
+        if (connection) right = EMPTY_V_WALL;
+        else right = V_WALL;
+    }
+
+    public void setEnd(boolean e) {
+        isEnd = e;
+    }
+
+    public void setNumber(int n) {
+        number = n;
+    }
+
 }
--- src/main/java/main/mazes/Maze.java
+++ src/main/java/main/mazes/Maze.java
@@ -5,12 +5,15 @@ import main.graphs.AdjacencyListGraph;
 import java.util.Random;
 
 public class Maze {
-    private int length, height;
-    private AdjacencyListGraph maze;
+    private final int length;
+    private final int height;
+    private final AdjacencyListGraph maze;
+    private final Cell[][] mazeCells;
     public Maze(int length, int height) {
         this.length = length;
         this.height = height;
         maze = new AdjacencyListGraph(this.length * this.height);
+        mazeCells = new Cell[this.length][this.height];
         generate(0,0);
     }
 
@@ -34,6 +37,12 @@ public class Maze {
                 generate(nextX, nextY);
             }
         }
+
+        for (y = 0; y < height; y++) {
+            for (x = 0; x < length; x++) {
+                createMazeWalls(x, y);
+            }
+        }
     }
     private int position(int x, int y) {
         if (y == 0) return x;
@@ -45,10 +54,36 @@ public class Maze {
 
     public void display() {
         System.out.println(maze);
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < height; j++) {
-
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < length; x++) {
+                System.out.print(mazeCells[x][y].getTop());
+            }
+            for (int x = 0; x < length; x++) {
+                System.out.print(mazeCells[x][y].getMiddle());
             }
         }
+        // print bottom row
+        for (int x = 0; x < length; x++) {
+            System.out.print(mazeCells[x][height - 1].getBottom());
+        }
+    }
+
+    private void createMazeWalls(int x, int y) {
+        int current = position(x, y);
+        int top = position(x, y - 1);
+        int bottom = position(x, y + 1);
+        int left = position(x - 1, y);
+        int right = position(x + 1, y);
+        mazeCells[x][y] = new Cell();
+        mazeCells[x][y].setNumber(current);
+        if (neighborExists(y + Directions.up.dy, height))
+            mazeCells[x][y].setTopConnection(maze.searchEdge(current,top));
+        if (neighborExists(y + Directions.down.dy, height))
+            mazeCells[x][y].setBottomConnection(maze.searchEdge(current,bottom));
+        if (neighborExists(x + Directions.left.dx, length))
+            mazeCells[x][y].setLeftConnection(maze.searchEdge(current,left));
+        if (neighborExists(x + Directions.right.dx, length))
+            mazeCells[x][y].setRightConnection(maze.searchEdge(current,right));
+        if (x == length - 1) mazeCells[x][y].setEnd(true);
     }
 }
