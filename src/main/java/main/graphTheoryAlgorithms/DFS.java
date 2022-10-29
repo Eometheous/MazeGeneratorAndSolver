3d1ebe9c25fa13a248c274c117f20b8a2bc5f6bd
Eometheous <jonathan.sthomas@icloud.com>
2022-10-28 17:42:33 -0700

    Now displays how many tiles have been visited and puts # for solution path

--- src/main/java/main/graphTheoryAlgorithms/DFS.java
+++ src/main/java/main/graphTheoryAlgorithms/DFS.java
@@ -13,7 +13,7 @@ public class DFS {
      * @param currentPosition   current position in the maze
      * @return                  the solution of the maze
      */
-    public static LinkedList DepthFirstSearch(Maze maze, boolean[][] visited, Coordinate currentPosition) {
+    public static LinkedList DepthFirstSearch(Maze maze, boolean[][] visited, Coordinate currentPosition, int tilesVisited) {
         int x = currentPosition.getX();
         int y = currentPosition.getY();
         int currentNode = maze.node(x,y);
@@ -21,18 +21,24 @@ public class DFS {
         // grab next tile from maze at current node and store it
         Node nextTile = maze.getMazeGraph().getAdjList()[currentNode].getHead();
         while (nextTile != null) {
+            if (tilesVisited > 9) tilesVisited = 0;
+
+            maze.getMazeWalls()[currentPosition.getX()][currentPosition.getY()].setChar(String.valueOf(tilesVisited));
+            tilesVisited++;
+
             // grab next coordinates
             Coordinate nextTileCoords = maze.position(nextTile.getItem());
 
             // if next tile is the end of the maze
             if (nextTile.getItem() == maze.getEndingTile()) {
+                maze.getMazeWalls()[nextTileCoords.getX()][nextTileCoords.getY()].setChar(String.valueOf(tilesVisited));
                 return backTrack(maze, nextTileCoords);
             }
 
             // if current position hasn't been visited yet
             if (!visited[x][y]) {
                 visited[x][y] = true;
-                return DepthFirstSearch(maze, visited, nextTileCoords);
+                return DepthFirstSearch(maze, visited, nextTileCoords, tilesVisited);
             }
             nextTile = nextTile.getNext();
         }
@@ -46,16 +52,22 @@ public class DFS {
      * @return                  the solution path of the maze
      */
     private static LinkedList backTrack(Maze maze, Coordinate currentPosition) {
+        maze.display();
+        maze.getMazeWalls()[currentPosition.getX()][currentPosition.getY()].setChar("#");
         Node tile = maze.getMazeGraph().getAdjList()[maze.node(currentPosition.getX(), currentPosition.getY())].getHead();
         LinkedList path = new LinkedList();
         path.add(maze.getEndingTile());
         while (tile.getItem() != maze.getStartingTile()) {
             if (tile.getNext() == null){
                 path.add(tile.getItem());
+                currentPosition = maze.position(tile.getItem());
+                maze.getMazeWalls()[currentPosition.getX()][currentPosition.getY()].setChar("#");
                 tile = maze.getMazeGraph().getAdjList()[tile.getItem()].getHead();
             }
             tile = tile.getNext();
         }
+        currentPosition = maze.position(maze.getStartingTile());
+        maze.getMazeWalls()[currentPosition.getX()][currentPosition.getY()].setChar("#");
         path.add(maze.getStartingTile());
         return path;
     }
