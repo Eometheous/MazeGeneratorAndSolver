b99534578668cc369890dfc3a3c0e06807c42857
Eometheous <jonathan.sthomas@icloud.com>
2022-11-14 08:59:00 -0800

    removed unused getters and setters

--- src/main/java/main/mazes/Coordinate.java
+++ src/main/java/main/mazes/Coordinate.java
@@ -10,16 +10,9 @@ public class Coordinate {
     }
 
     public int getX() {return x;}
-
     public int getY() {return y;}
-
     public void setX(int x) {this.x = x;}
-
     public void setY(int y) {this.y = y;}
-
-    public boolean getVisited() { return visited; }
-    public void setVisited(boolean visited) { this.visited = visited; }
-
     public String toString() {
         return String.format("XY: %d %d", x, y);
     }
