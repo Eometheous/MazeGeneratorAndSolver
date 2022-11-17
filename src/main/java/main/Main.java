14bf5a7245f839e722885dd8704c6ae0e4db9490
Eometheous <jonathan@sthomasphoto.com>
2022-11-16 16:03:10 -0800

    Updated to create a random maze with length 3 and height 4

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,12 @@
 package main;
 
 import main.mazes.Maze;
+import main.mazes.MazeSolver;
 
 public class Main {
     public static void main(String[] args) {
-        Maze maze24 = new Maze(3, 4);
-        maze24.display();
+        Maze maze34 = new Maze(3, 4);
+        MazeSolver.solveMazeUsingDFS(maze34);
+        MazeSolver.solveMazeUsingBFS(maze34);
     }
 }
