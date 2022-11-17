431edd0dd5f9f92d56fc2a6b583a8bfc8da7e739
SpinnerX <heraaronhotmail@yahoo.com>
2022-11-16 18:52:00 -0800

    Made some changes

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,8 +5,7 @@ import main.mazes.MazeSolver;
 
 public class Main {
     public static void main(String[] args) {
-        Maze maze34 = new Maze(3, 4);
-        MazeSolver.solveMazeUsingDFS(maze34);
-        MazeSolver.solveMazeUsingBFS(maze34);
+        Maze maze24 = new Maze(3, 4);
+        MazeSolver.solveMazeUsingBFS(maze24);
     }
 }
