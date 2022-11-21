1e20cc189e897ec4d9296d1280f494b6f9dfae22
Eometheous <jonathan.sthomas@icloud.com>
2022-11-20 20:34:32 -0800

    Got rid of weird spacing

--- src/main/java/main/mazes/Directions.java
+++ src/main/java/main/mazes/Directions.java
@@ -1,13 +1,10 @@
 package main.mazes;
 
 public enum Directions {
-
-
     up(0, -1), down(0,1), left(-1, 0), right(1, 0);
     public final int dx, dy;
-
     Directions(int dx, int dy) {
         this.dx = dx;
         this.dy = dy;
     }
-};
+}
