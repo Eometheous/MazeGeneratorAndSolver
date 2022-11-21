c6d249ea5441ba6909154a56755beb76836067a6
Eometheous <jonathan.sthomas@icloud.com>
2022-11-20 20:32:25 -0800

    Called display within maze class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,7 +6,9 @@ import main.mazes.MazeSolver;
 public class Main {
     public static void main(String[] args) {
         Maze maze34 = new Maze(3, 4);
+        maze34.display();
         MazeSolver.solveMazeUsingDFS(maze34);
         MazeSolver.solveMazeUsingBFS(maze34);
+        maze34.display();
     }
 }
